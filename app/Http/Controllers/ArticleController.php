d43e908a1d69f56495ffb29b6fabc316ff6d4ba2
vlain01 <waiyankyw96@gmail.com>
2022-02-15 19:53:50 +0630

    slutErrorFIx

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -110,8 +110,10 @@ class ArticleController extends Controller
             "description" => "required|min:5"
         ]);
         
+        if($article->title != $request->title){
+            $article->slug = Str::slug($request->title)."-".uniqid() ;
+        }
         $article->title = $request->title ;
-        $article->slug = Str::slug($request->title)."-".uniqid() ;
         $article->description = $request->description ;
         $article->category_id = $request->category ;
         $article->category_slug = $article->category->title ;
